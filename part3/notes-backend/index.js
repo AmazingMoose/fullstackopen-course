9520e0b673f66bd3ffef1b4e1a98a24bc669535f
Daniil <zykindawork@gmail.com>
2025-05-23 11:14:23 +0300

    Add part3 notes-frontend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const requestLogger = (request, response, next) => {
@@ -27,6 +28,7 @@ let notes = [
     }
 ]
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -90,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
